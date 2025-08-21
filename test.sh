707b175c78e0ec0ecafff267f0d1acc9fb502a42
Gilad Barnea <giladbrn@gmail.com>
2025-08-21 17:24:44 +0200

    wip

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 if [[ -t 1 && -t 0 && "$USER" = giladbarnea && "$LOGNAME" = giladbarnea && "$CURSOR_AGENT" != 1 ]]; then
-    uv run python -m pytest tests --color=yes --code-highlight=yes "$@"
+    uv run python -m pytest --color=yes --code-highlight=yes "$@"
 else
-    uv run python -m pytest tests --color=no --code-highlight=no -vv "$@"
+    uv run python -m pytest --color=no --code-highlight=no -vv "$@"
 fi
